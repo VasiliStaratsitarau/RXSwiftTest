5539e286cb50e7bb0b5260f09e105716f7d340ca
Vasili <v.staratsitarau@gmail.com>
2022-09-16 11:13:42 +0200

    remove search logic in extension

---
+++ RXSwift_test/RXSwift_test/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RXSwift_test
+//
+//  Created by Vasili Staratsitarau on 16/09/2022.
+//
+
+import UIKit
+import RxSwift
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+}
+
